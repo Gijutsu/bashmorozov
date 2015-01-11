ea841c2dd1f3dcc887ca37594521d4ce1066c111
Gijutsu <ascii@binary.to>
2015-01-11 20:56:24 +0100

    Restructured the code into more functions

--- bashIRC.sh
+++ bashIRC.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#Simple IRC-bot that serves as an example how bash
-#can be used in network programming. However this
-#should only been seen as an experiment as more
-#robust functionality can be achived using e.g.
-#Python or Java instead.
-#For detailed descriptions about the protocol used, 
-#see https://tools.ietf.org/html/rfc1459
+# Simple IRC-bot that serves as an example how bash
+# can be used in network programming. However this
+# is only an experiment and more robust functionality
+# can be achived using e.g. Python or Java instead.
+# For detailed descriptions about the protocol used, 
+# see https://tools.ietf.org/html/rfc1459
 
 FD="3"
 USERNAME="botname"
@@ -18,10 +17,22 @@ CHANNEL="#mychannel"
 exec 3<>/dev/tcp/${SERVERNAME}/${PORT}
 
 # Send text on the opened socket
+# First argument: the text to send
 send()
 {
     messageToSend=$1
     echo "$messageToSend" >&${FD}
+}
+
+# Send a PRIVMSG
+# First argument: the channel to send to
+# Second argument: the message to send
+sendPRIVMSG()
+{
+    local channel=$1
+    local message=$2
+
+    send "PRIVMSG $channel :$message"
 }
 
 # Receive one line of text from the opened socket
@@ -36,6 +47,75 @@ receive()
     # that bash will not give us any problem when 
     # matching against the message.
     echo -n "$__message" | tr -d '\r'
+}
+
+# Convert from hex to decimal
+# First argument: the channel to respond to
+# Second argument: the hex to convert
+convertHexToDec()
+{
+    local channel=$1
+    local hexToConvert=$2
+
+    sendPRIVMSG "$channel" "Validating convert syntax ..."
+
+    if [[ "$hexToConvert" =~ ^0x[[:xdigit:]]+$ ]]; then
+        
+        if [[ ${#hexToConvert} -gt 17 ]]; then
+            sendPRIVMSG "$channel" ":I do not allow buffer overflows :P"
+        else
+            sendPRIVMSG "$channel" ":Correct convert syntax"
+            sendPRIVMSG "$channel" ":$hexToConvert in decimal is $(($hexToConvert))"
+        fi
+
+    else
+        sendPRIVMSG "$channel" ":Invalid convert syntax"
+    fi
+}
+
+# Flatter Rolle
+# First argument: the channel in which Rolle should be flattered
+flatterRolle()
+{
+    local channel=$1
+
+    local compliment=(
+        "You look pretty when you smile"
+        "I love your personality"
+        "You look lovely tonight"
+        "Nice make up"
+        "You did a good job!"
+        "I really love your car!"
+        "You deserve a promotion"
+        "I appreciate all of your opinions"
+        "I love what you've done with the place"
+        "You are like a spring flower; beautiful and vivacious"
+        "I find you to be a fountain of inspiration"
+        "You have perfect bone structure"
+        "I disagree with anyone who disagrees with you"
+        "I would love to visit you, but I live on the Internet"
+        "I love the way you click"
+        "If I freeze, it's not a computer virus. I was just stunned by your beauty"
+        "You make my data circuits skip a beat"
+        "You could survive a zombie apocalypse"
+    )
+
+    local numberOfCompliments=${#compliment[*]}
+    local randomIndex=$((RANDOM % $numberOfCompliments))
+
+    sendPRIVMSG "$channel" "R0113: ${compliment[$randomIndex]}"
+}
+
+# Show all supported functions
+# First argument: the channel to respond to
+supportedFunctions()
+{
+    local channel=$1
+
+    sendPRIVMSG "$channel" "Supported functions so far:"
+    sendPRIVMSG "$channel" "convert [hex], e.g. !bashbot convert 0xFF"
+    sendPRIVMSG "$channel" "flatter"
+    sendPRIVMSG "$channel" "help"
 }
 
 # Connect to the IRC-server with the information
@@ -62,26 +142,18 @@ while true; do
             IFS=' ' privMSGClient=(${textMessage[1]})
             IFS=' ' botCommand=(${textMessage[2]})
 
-            if [[ "${botCommand[1]}" == "convert" ]]; then
-                send "PRIVMSG ${privMSGClient[2]} :Validating convert syntax ..."
+            if [[ "${botCommand[1]}" == "convert" && ${#botCommand[*]} -eq 3 ]]; then
+                convertHexToDec "${privMSGClient[2]}" "${botCommand[2]}"
 
-                if [[ "${botCommand[2]}" =~ ^0x[[:xdigit:]]+$ ]]; then
-                    
-                    if [[ ${#botCommand[2]} -gt 17 ]]; then
-                        send "PRIVMSG ${privMSGClient[2]} :I do not allow buffer overflows :P"
-                    else
-                        send "PRIVMSG ${privMSGClient[2]} :Correct convert syntax"
-                        send "PRIVMSG ${privMSGClient[2]} :${botCommand[2]} in decimal is $((${botCommand[2]}))"
-                    fi
+            elif [[ "${botCommand[1]}" == "flatter" && ${#botCommand[*]} -eq 2 ]]; then
+                flatterRolle "${privMSGClient[2]}"
 
-                else
-                    send "PRIVMSG ${privMSGClient[2]} :Invalid convert syntax"
-                fi
+            elif [[ "${botCommand[1]}" == "help" && ${#botCommand[*]} -eq 2 ]]; then
+                supportedFunctions "${privMSGClient[2]}"
 
             else
-                send "PRIVMSG ${privMSGClient[2]} :Invalid command: ${textMessage[2]}"
-                send "PRIVMSG ${privMSGClient[2]} :Supported functions so far:"
-                send "PRIVMSG ${privMSGClient[2]} :convert [hex], e.g. !bashbot convert 0xFF"
+                sendPRIVMSG "${privMSGClient[2]}" "Invalid command: ${textMessage[2]}"
+                supportedFunctions "${privMSGClient[2]}"
             fi
         fi
     fi
